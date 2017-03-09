64b5ee8d1a3b491559a0968200b124254dec3dc5
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2017-03-09 15:08:11 -0600

    Add scoring function to game tree

--- test/tic_tac_toe/artificial_intelligence/game_tree_test.exs
+++ test/tic_tac_toe/artificial_intelligence/game_tree_test.exs
@@ -46,4 +46,142 @@ defmodule GameTreeTest do
 
   end
 
+  describe "scoring a game tree" do
+
+    test "returns the value of a node if it has no children" do
+      assert Tree.score(Tree.node(51)) == 51
+    end
+
+    test "returns the value of a node, even if the value is negative" do
+      assert Tree.score(Tree.node(-2)) == -2
+    end
+
+    test "returns the value of the child of a node if the node has nil value" do
+      node = Tree.add_child(Tree.node, Tree.node(8))
+      assert Tree.score(node) == 8
+    end
+
+    test "returns the maximum of the values of its children (max first)" do
+      tree = Tree.node
+      |> Tree.add_child(Tree.node(5))
+      |> Tree.add_child(Tree.node(1))
+      |> Tree.add_child(Tree.node(-3))
+      assert Tree.score(tree) == 5
+    end
+
+    test "returns the maximum of the values of its children (max last)" do
+      tree = Tree.node
+      |> Tree.add_child(Tree.node(5))
+      |> Tree.add_child(Tree.node(1))
+      |> Tree.add_child(Tree.node(38))
+      assert Tree.score(tree) == 38
+    end
+
+    test "returns the maximum of the values of its children (negative values)" do
+      tree = Tree.node
+      |> Tree.add_child(Tree.node(-15))
+      |> Tree.add_child(Tree.node(-212))
+      |> Tree.add_child(Tree.node(-38))
+      assert Tree.score(tree) == -15
+    end
+
+    test "returns the maximum of the values of its children (duplicate values)" do
+      tree = Tree.node
+      |> Tree.add_child(Tree.node(1))
+      |> Tree.add_child(Tree.node(1))
+      |> Tree.add_child(Tree.node(1))
+      assert Tree.score(tree) == 1
+    end
+
+    test "returns the value of a single grandchild if it is the only node with a value" do
+      middle_node = Tree.add_child(Tree.node, Tree.node(14))
+      tree = Tree.add_child(Tree.node, middle_node)
+      assert Tree.score(tree) == 14
+    end
+
+    test "returns the minimum value of all the grandchildren" do
+      middle_node = Tree.node
+      |> Tree.add_child(Tree.node(124))
+      |> Tree.add_child(Tree.node(12))
+      |> Tree.add_child(Tree.node(6))
+      tree = Tree.add_child(Tree.node, middle_node)
+      assert Tree.score(tree) == 6
+    end
+
+    test "it returns the score from a child if it is better than a grandchild" do
+      child1 = Tree.node
+      |> Tree.add_child(Tree.node(12))
+      |> Tree.add_child(Tree.node(12))
+      |> Tree.add_child(Tree.node(6))
+      child2 = Tree.node(8)
+      tree = Tree.node
+      |> Tree.add_child(child1)
+      |> Tree.add_child(child2)
+      assert Tree.score(tree) == 8
+    end
+
+    test "it correctly traverses two sets of grandchildren" do
+      child1 = Tree.node
+      |> Tree.add_child(Tree.node(0))
+      |> Tree.add_child(Tree.node(-12))
+      |> Tree.add_child(Tree.node(60))
+      child2 = Tree.node
+      |> Tree.add_child(Tree.node(2))
+      |> Tree.add_child(Tree.node(25))
+      |> Tree.add_child(Tree.node(-6))
+      tree = Tree.node
+      |> Tree.add_child(child1)
+      |> Tree.add_child(child2)
+      assert Tree.score(tree) == -6
+    end
+
+    test "returns the score for a complicated game tree" do
+      d1 = Tree.node(2)
+      d2 = Tree.node(-1)
+      d3 = Tree.node(3)
+      d4 = Tree.node(6)
+      d5 = Tree.node(0)
+      d6 = Tree.node(0)
+      d7 = Tree.node(4)
+      d8 = Tree.node(3)
+
+      c1 = Tree.node
+      |> Tree.add_child(d1)
+      |> Tree.add_child(d2)
+      c2 = Tree.node
+      |> Tree.add_child(d3)
+      c3 = Tree.node
+      |> Tree.add_child(d4)
+      |> Tree.add_child(d5)
+      c4 = Tree.node
+      |> Tree.add_child(d6)
+      |> Tree.add_child(d7)
+      c5 = Tree.node
+      |> Tree.add_child(d8)
+      c6 = Tree.node(7)
+      c7 = Tree.node(1)
+
+      b1 = Tree.node
+      |> Tree.add_child(c1)
+      b2 = Tree.node
+      |> Tree.add_child(c2)
+      |> Tree.add_child(c3)
+      b3 = Tree.node
+      |> Tree.add_child(c4)
+      b4 = Tree.node
+      |> Tree.add_child(c5)
+      |> Tree.add_child(c6)
+      |> Tree.add_child(c7)
+
+      a1 = Tree.node
+      |> Tree.add_child(b1)
+      |> Tree.add_child(b2)
+      |> Tree.add_child(b3)
+      |> Tree.add_child(b4)
+
+      assert Tree.score(a1) == 4
+    end
+
+  end
+
 end
